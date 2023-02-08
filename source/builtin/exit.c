d4ccee74cf14b4fbaf5632df99b9bbe237d44c03
gusousa <gfsousa42@gmail.com>
2023-02-08 00:17:06 -0300

    WIP: exit ideation

--- source/builtin/exit.c
+++ source/builtin/exit.c
@@ -6,18 +6,30 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:46:15 by gusousa           #+#    #+#             */
-/*   Updated: 2023/02/07 15:54:23 by gusousa          ###   ########.fr       */
+/*   Updated: 2023/02/07 22:06:09 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+int	g_status = 0;
+
 void	ft_exit(char *str)
 {
 	int	nbr_child;
 	int	id;
-
-	nbr_child
-	id = ft_atoi(str);
-	exit(0);
+	
+	if (qtd_exit > 1)
+	{
+		// criar fork.
+		id = ft_atoi(str);
+		g_status = id;
+		ft_putstr_fd("exit\n", 1);
+		exit(id);
+	}
+	else
+	{
+		ft_putstr_fd("exit\n", 1);
+		exit(0);
+	}
 }
--- source/main_tests.c
+++ source/main_tests.c
@@ -3,8 +3,29 @@
 
 int	main(void)
 {
-	//char	str[] = "-n -nnnn -nnnnnnn It's gonna be a long long time";
-	char	str[] = "";
+	char	str[] = "-n -nnnn -nnnnnnn It's gonna be a long long time";
 	ft_echo(str);
+	char	str2[] = "-ntnnn you can't just leave me breath?";
+	ft_echo(str2);
+	char	str1[] = "";
+	ft_echo(str1);
+
+///		Exit
+		/*
+	ft_exit("42");
+	ft_exit("42 53 68");
+	ft_exit("259");
+	ft_exit("9223372036854775807");
+	ft_exit("-9223372036854775808");
+	ft_exit("9223372036854775808");
+	ft_exit("-9223372036854775810");
+	ft_exit("-4");
+	ft_exit("wrong");
+	ft_exit("exit wrong_command");
+	ft_exit("gdagadgag");
+	ft_exit("ls -Z");
+	ft_exit("cd gdhahahad");
+	ft_exit("ls -la | wtf");
+	*/
 	return (0);
 }

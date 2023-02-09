7afd97c20d2953bc0c92710fbbe92cb4d40280b9
gusousa <gfsousa42@gmail.com>
2023-02-09 12:19:39 -0300

    WIP: considering exit with too many arguments

--- source/builtin/exit.c
+++ source/builtin/exit.c
@@ -1,16 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exit_extra.c                                       :+:      :+:    :+:   */
+/*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 15:46:15 by gusousa           #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/02/09 11:06:05 by gusousa          ###   ########.fr       */
-=======
-/*   Updated: 2023/02/08 14:44:40 by gusousa          ###   ########.fr       */
->>>>>>> b70911a71541b2925a87d4979581b49f76c043ab
+/*   Updated: 2023/02/09 12:18:43 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +17,11 @@ int	g_status = 0;
 int	is_numeric(char *str)
 {
 	char	**str_mtx;
+	int		i;
+	int		j;
 
 	str_mtx = ft_split(str, ' ');
+	i = 0;
 	while (str[i])
 	{
 		j = 0;
@@ -61,7 +60,15 @@ void	ft_exit(char *str)
 	int				qtd_exit;
 	
 	qtd_exit = count_words(str) + 1;
-	if (qtd_exit > 1)
+	if (qtd_exit == 1)
+	{
+		ft_putstr_fd(str, 1);
+		ft_putstr_fd("exit with no args\n", 1);
+		//exit(0);
+
+
+	}
+	else if (qtd_exit == 2)
 	{
 		// criar fork.
 		if (is_numeric(str))
@@ -81,11 +88,13 @@ void	ft_exit(char *str)
 			g_status = 255;
 			//exit(255);
 		}
+
 	}
 	else
 	{
-		ft_putstr_fd(str, 1);
-		ft_putstr_fd("exit with no args\n", 1);
-		//exit(0);
+			ft_putstr_fd(str, 1);
+			ft_putstr_fd("\tExit: too many arguments\n", 2);
+			g_status = 1;
+			//exit(255);
 	}
 }
--- test_echo.c
+++
@@ -1,9 +0,0 @@
-
-void    ft_echo(char *str);
-
-int main(void)
-{
-    ft_echo("-n teste echo teste");
-    ft_echo("teste echo teste");
-    return (0);
-}

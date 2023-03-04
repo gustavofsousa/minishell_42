5ec6543e8024ac21471774da297f389f89f6960f
Pedro Arnaldo <parnaldo@student.42.rio>
2023-03-04 18:02:47 -0300

    create checking quotes

--- source/expand_variables.c
+++ source/expand_variables.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 10:57:41 by gusousa           #+#    #+#             */
-/*   Updated: 2023/03/03 17:27:47 by parnaldo         ###   ########.fr       */
+/*   Updated: 2023/03/04 17:58:13 by parnaldo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,75 +15,124 @@
 /*
  * Apaga onde estava o nome da variável e coloca o valor dela.
  */
+
+
+int len_word(char *str)
+{
+	int start;
+	int	end;
+
+	start = 0;
+	end = 0;
+	if (!str)
+		return (-1);
+	while (str[start] != '$')
+		start++;
+	end = start;
+	while ((str[end] != 34 && str[end] != 39) && str[end])
+		end++;
+	return (end - start);
+}
+
 void	substitute(t_cell **list, t_info info, int index)
 {
-	int len;
+	int len_content;
+	int len_w;
+	int len_new_content;
+	int	i;
+	int aux;
+	char *str;
 
-	len = 0;
-	len = ft_strlen((*list)->content);
+	i = 0;
+	aux = 0;
+	len_content = 0;
+	len_w = 0;
+	len_new_content = 0;
+	while (info.env_cpy[index][len_w] != '=')
+		len_w++;
+	len_content = ft_strlen((*list)->content) - (len_w + 1);
+	printf("%d\n", len_content);
+	len_new_content = ft_strlen(info.env_cpy[index]) - len_w;
+	len_new_content += len_content;
+	printf("len total %d\n", len_new_content);
+	str = malloc(len_new_content * sizeof(char));
+	while ((*list)->content[i] != '$')
+	{
+		str[i] = (*list)->content[i];
+		i++;
+	}
+	aux = i+(len_w+1);
+	while (info.env_cpy[index][++len_w])
+		str[i++] = info.env_cpy[index][len_w];
+	while ((*list)->content[aux])
+         str[i++] = (*list)->content[aux++];
 	free((*list)->content);
 	(*list)->content = NULL;
-	(*list)->content = ft_strdup(info.env_cpy[index]+len);
+	(*list)->content = str;
+	printf("list->content = %s\n", (*list)->content);
 }
 
 /*
  * Compara se depois do $ a variável está com nome igual.
  */
+
 int	look_for_variable(t_cell *list, t_info info)
 {	
-	int	i;
+	int	index;
+	char *str;
+	char *str_env;
 	int len;
+	int len_w;
 
-	//verificar se tem essa variavel na env, se sim retornar o index
-	i = 0;
 	len = 0;
-	while (list != NULL)
-	{
-		i = 0;
-		while (info.env_cpy[i])
-		{
-			len = ft_strlen(list->content);
-			if(!ft_strncmp(list->content+1, info.env_cpy[i], len-1))
-			{
-				return (i);
-			}
-			 i++;
-		}
-		list = list->next;
-	}
-	return (0);
-}
-
-int	check_quotes(t_cell **list)
-{
-	int	i;
-	char str;
-
-	i = 0;
+	len_w = 0;
+	index = 0;
 	str = list->content;
-
-	
+	len_w = len_word(str);
+	while (str[index] != '$')
+		index++;
+	str_env = ft_substr(str, index, len_w);
+	index = 0;
+	while (info.env_cpy[index])
+	{
+		len = ft_strlen(str_env+1);
+		if (!ft_strncmp(str_env+1, info.env_cpy[index], len))
+		{
+			printf("%s\n", info.env_cpy[index]+(len+1));
+			return (index);
+		}
+		index++;
+	}
+	free(str_env);
+	return (1);
 }
 
 void	expand_variable(t_cell **list_cell, t_info info)
 {
 	t_cell	*list_move;
 	int		index;
+	int		is_quotes;
 
+	is_quotes = 0;
 	index = 0;
 	list_move = *list_cell;
 	while (list_move != NULL)
 	{
-		//check quotes
+		//vai ter duas condições e a com aspas
 		printf("%s\n", list_move->content);
 		if (ft_strchr(list_move->content, '$'))
 		{
+			if (ft_strchr(list_move->content, 39))
+			{
+				if (!ft_strchr(list_move->content, 34))
+					return ;
+			}
 			index = look_for_variable(list_move, info);
 			if (index)
 			{
 				substitute(&list_move, info, index);
 			}
-		}
+		}	
 		list_move = list_move->next;
 	}
 }

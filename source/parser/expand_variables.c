5514a9cafcd0921c6ef4b5b6d1a4d3eb7efd1768
gusousa <gfsousa42@gmail.com>
2023-03-21 16:22:15 -0300

    returnright

--- source/parser/expand_variables.c
+++ source/parser/expand_variables.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 10:57:41 by gusousa           #+#    #+#             */
-/*   Updated: 2023/03/21 16:18:59 by gusousa          ###   ########.fr       */
+/*   Updated: 2023/03/21 16:21:18 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,7 @@ int	get_value_env(t_info info, t_cell **list,  char *str, int i)
 		}
 	}
 	dup_or_join_string(list, new_value);
-	return (len);
+	return (len + 1);
 }
 
 int	change_flag_quote(char c, int fq)

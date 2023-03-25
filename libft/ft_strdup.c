bf323df097b6d09aa44208368ded3d0d283bfd61
gusousa <gfsousa42@gmail.com>
2023-03-25 17:52:15 -0300

    fix: segfault in quotes

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: gusousa <gusousa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 10:50:18 by gusousa           #+#    #+#             */
-/*   Updated: 2022/05/26 10:02:20 by gusousa          ###   ########.fr       */
+/*   Updated: 2023/03/25 15:31:04 by gusousa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strdup(const char *s1)
 	char	*my_copy;
 	size_t	len;
 
+	if (s1 == NULL)
+		return (NULL);
 	len = ft_strlen(s1);
 	my_copy = malloc(len + 1);
 	if (!my_copy)
